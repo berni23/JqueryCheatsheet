998c7f8b6b893175e4edb52b36a91f6dcdc237ee
Berni <bernatferrerm@gmail.com>
2020-09-09 13:46:23 +0200

    code commented

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
-
-
-
     let infoWindow = document.querySelector('.info-window');
     let infoTimer;
 
+    // v-n means dealing with exercise n in javascript vanilla
+
+
+    /*------------------------------
+    1-Events
+    ----------------------------*/
+
+    // v1 ---------------------------------
+
     message("vanilla script ready")
 
+    // v2 ---------------------------------
 
     document.querySelector(".v-2").addEventListener("click", () => message("vanilla element clicked!"))
 
+    // v3 ---------------------------------
+
     document.querySelector(".v-3").addEventListener("dblclick", () => message(" vanilla element double clicked!"))
 
+    // v4 ---------------------------------
     document.addEventListener('keydown', () => document.querySelector('.v-4').textContent = (event.keyCode || event.which))
 
+
+    // v5 ---------------------------------
     document.querySelector(".v-5").addEventListener('mousemove',
 
         function (event) {
@@ -30,13 +42,14 @@ document.addEventListener("DOMContentLoaded", function () {
     )
 
 
+    // v6 ---------------------------------
     let v6 = document.querySelector(".v-6")
     v6.addEventListener("input", () => message("User said: " + v6.value))
 
+    // v7-8 ---------------------------------
     let arrV7 = ["images/earth.jpg", "images/moon.jpg", "images/saturn.jpeg"]
     let imgV7 = 0
     let imgE7 = document.querySelector(".img-e7")
-
 
     imgE7.addEventListener("load", () => message("image succesfully loaded !"))
 
@@ -52,16 +65,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
+    // v9 ---------------------------------
     document.querySelector(".v-9").addEventListener("submit", () => message("form submitted!"))
 
+    // v10 ---------------------------------
     let v10 = document.querySelector(".v-10")
     v10.addEventListener("change", () => message(" you choosed " + v10.value))
 
 
+    // v11 ---------------------------------
     let v11 = document.querySelector(".v-11")
 
     v11.addEventListener("mouseover", () => v11.classList.toggle("v11-hover1"))
 
+    // v12 ---------------------------------
     document.querySelector(".v-12").addEventListener('change', function () {
 
         let value = ""
@@ -71,14 +88,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
+    // v13 ---------------------------------
     document.querySelector(".v-13").addEventListener("click", function (event) {
         if (event.target != event.currentTarget) {
             message(event.target.textContent + " clicked!")
         }
     })
 
-    /* 2 - functios and selectors*/
+    /*----------------------------------
+        2 - functios and selectors
+    ---------------------------------*/
 
+
+    // v14---------------------------------
     listF14 = document.querySelector(".ul-f14")
 
     document.querySelector(".v-14").addEventListener("click", function () {
@@ -89,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function () {
         message("element appended using vanilla")
     })
 
-
+    // v15---------------------------------
     document.querySelector('.v-15').addEventListener("click", function () {
 
         if (listF14.children.length) {
@@ -100,6 +122,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
+    // v16---------------------------------
     document.querySelector(".v-16").addEventListener("click", function () {
         let prepended = listF14.children.length + 1
         let child = document.createElement('li')
@@ -108,10 +131,9 @@ document.addEventListener("DOMContentLoaded", function () {
         message("element prepended using vanilla")
     })
 
-
+    // v17---------------------------------
     let liF17 = document.querySelector(".li-f17")
     let ulF17 = document.querySelector(".ul-f17")
-
 
     document.querySelector(".v-17").addEventListener("click", function () {
 
@@ -121,11 +143,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
-
     function insertAfter(newNode, referenceNode) {
         referenceNode.parentElement.insertBefore(newNode, referenceNode.nextSibling);
     }
 
+    // v18---------------------------------
     document.querySelector(".v-18").addEventListener("click", function () {
 
         let after = document.createElement('li')
@@ -134,6 +156,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
+    // v19---------------------------------
     let liF19 = document.querySelector(".li-f19")
     let ulF19 = document.querySelector(".ul-f19")
 
@@ -144,22 +167,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
+    // v20---------------------------------
     let v20Div = document.querySelector(".v-20-div");
 
     document.querySelector(".v-20").addEventListener("click", () => v20Div.classList.add("class20"))
+    // v21---------------------------------
     document.querySelector(".v-21").addEventListener("click", () => v20Div.classList.remove("class20"))
+    // v22---------------------------------
     document.querySelector(".v-22").addEventListener("click", () => v20Div.classList.toggle("class20"))
 
+    // v23---------------------------------
     let v23Btn = document.querySelector(".v-23-btn")
 
     document.querySelector(".v-23").addEventListener("click", () => v23Btn.setAttribute('disabled', true))
 
     v23Btn.addEventListener("click", () => message("I'm not disabled =)"))
 
+    // v24---------------------------------
     document.querySelector(".v-24").addEventListener("click", () => v23Btn.removeAttribute('disabled'))
 
-
-
+    // v25---------------------------------
     let f25Img = document.querySelector('.f-25-img');
 
     document.querySelector(".v-25").addEventListener("click", function () {
@@ -168,17 +195,21 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 
+    // v26---------------------------------
     document.querySelector(".v-26").addEventListener("click", function () {
         f25Img.removeAttribute("data-src", "images/monkey.png");
         message("data source removed")
 
     })
 
+    // v27---------------------------------
     let v27Div = document.querySelector(".v-27-div")
 
     document.querySelector(".v-27").addEventListener("click", () => v27Div.classList.add('hidden'))
+    // v28---------------------------------
     document.querySelector(".v-28").addEventListener("click", () => v27Div.classList.remove('hidden'))
-
+    //( 29 and 30 are only for jquery)
+    // v31---------------------------------
     let collectionJ31 = Array.from(document.getElementsByClassName("ref-31-div"));
 
     document.querySelector(".v-31").addEventListener("click", () => collectionJ31.forEach(function (el) {
@@ -241,9 +272,6 @@ document.addEventListener("DOMContentLoaded", function () {
         let input = document.querySelector('input');
         window.alert(input.getAttribute("value"))
     })
-
-
-
 
     document.querySelector('.v-40').addEventListener("click", function () {
 
--- script_jquery.js
+++ script_jquery.js
@@ -3,16 +3,24 @@ $(function () {
     let infoWindow = $('.jquery-info-window')
     let infoTimer;
 
-    /* 1 - events */
 
+    // j-n  label states for exercise n performed using jquery library
+
+    /*
+    ----------------------------
+    1 - Events
+    -----------------------------
+    */
+
+    //j1 ----------------------------
     message("jquery script ready")
-
+    //j2 ----------------------------
     $(".j-2").click(() => message("jquery element clicked! "))
-
+    //j3 ----------------------------
     $(".j-3").dblclick(() => message("jquery element double clicked!"))
-
+    //j4 ----------------------------
     $(window).keydown(() => $('.j-4').text(event.keyCode || event.which))
-
+    //j5 ----------------------------
     $(".j-5").mousemove(
 
         function (event) {
@@ -25,18 +33,20 @@ $(function () {
         }
     )
 
+    //j6 ----------------------------
     $(".j-6").on("input", () => message("User said : " + $('.j-6').val()))
-
+    //j7-8 ----------------------------
     $(".img-e7").on("load", () => message("image succesfully loaded"))
     $(".img-e7").on("error", () => {
 
         message("image could not be loaded")
     })
 
+    //j9 ----------------------------
     $(".j-9").submit(() => message("form submitted!"))
-
+    //j10 ----------------------------
     $(".j-10").change(() => message(" you choosed " + $(this).children().find(":selected").text()))
-
+    //j11 ----------------------------
     let move = true;
     $(".j-11").mouseover(function () {
 
@@ -52,7 +62,7 @@ $(function () {
             move = true
         }
     })
-
+    //j12 ----------------------------
     $(".j-12").change(function () {
 
         let value = ''
@@ -61,13 +71,17 @@ $(function () {
         message("checkbox " + value + "!")
 
     })
-
+    //j13 ----------------------------
     $(".j-13").click(function (event) {
 
         if (event.target != event.currentTarget) message(event.target.textContent + " clicked!")
     })
-    /* 2 - functions and selectors*/
 
+    /* -----------------------------------------
+    2 - functions and selectors
+    --------------------------------------*/
+
+    //j14 ----------------------------
     let listF14 = $('.ul-f14')
     $(".j-14").click(() => {
 
@@ -76,7 +90,7 @@ $(function () {
         message("element appended using jquery")
 
     })
-
+    //j15 ----------------------------
     $('.j-15').click(function () {
 
         if (listF14.children().length) {
@@ -85,23 +99,23 @@ $(function () {
             message("element removed using jquery")
         }
     })
-
+    //j16 ----------------------------
     $('.j-16').click(function () {
 
         let prepended = listF14.children().length + 1
         listF14.prepend($(`<li><div class = "jquery">${prepended} elements created</div></li>`))
         message("element prepended using jquery")
     })
-
+    //j17 ----------------------------
     liF17 = $('.li-f17')
     $(".j-17").click(function () {
         liF17.before($(`<li><div class = "jquery">added before</div></li>`))
     })
-
+    //j18 ----------------------------
     $(".j-18").click(function () {
         liF17.after($(`<li><div class = "jquery">added after</div></li>`))
     })
-
+    //j19 ----------------------------
     let ulF19 = $(".ul-f19");
     let liF19 = $(".li-f19");
     $(".j-19").click(function () {
@@ -109,28 +123,32 @@ $(function () {
         let cloned = liF19.clone()
         ulF19.append(cloned)
     })
-
+    //j20----------------------------
     let j20Div = $(".j-20-div");
 
     $(".j-20").click(() => j20Div.addClass("class20"))
+    //j21----------------------------
     $(".j-21").click(() => j20Div.removeClass("class20"))
+    //j22----------------------------
     $(".j-22").click(() => j20Div.toggleClass("class20"))
-
+    //j23----------------------------
     let j23Btn = $(".j-23-btn")
 
     $(".j-23").click(() => j23Btn.prop('disabled', true))
     $(".j-23-btn").click(() => message("im not disabled =)"))
+    //j24----------------------------
     $(".j-24").click(function () {
         j23Btn.prop('disabled', false)
         j23Btn.removeAttr('disabled')
     })
-
+    //j25----------------------------
     let f25Img = $('.f-25-img')
     $(".j-25").click(function () {
 
         f25Img.attr("data-src", "images/tiger.png");
         message("data source set to " + f25Img.attr("data-src"))
     })
+    //j26----------------------------
     $(".j-26").click(function () {
 
         f25Img.removeData('src')
@@ -138,17 +156,19 @@ $(function () {
         message("data source removed")
 
     })
-
+    //j27----------------------------
     let j27Div = $(".j-27-div")
     $(".j-27").click(() => j27Div.addClass('hidden'))
+    //j28----------------------------
     $(".j-28").click(() => j27Div.removeClass('hidden'))
-
+    //j29----------------------------
     let j29Div = $(".j-29-div")
 
     $(".j-29").click(() => j29Div.fadeIn(500))
+    //j30----------------------------
     $(".j-30").click(() => j29Div.fadeOut(500))
 
-
+    //j31----------------------------
     let collectionJ31 = $(".ref-31-div");
 
     $(".j-31").click(function () {
@@ -160,9 +180,10 @@ $(function () {
             })
         }
     })
-
+    //j32----------------------------
     let f32 = $(".f-32-span")
     $(".j-32").click(() => f32.parent().css("fontWeight", "bold"))
+    //j33----------------------------
     let f33 = $(".f-33-ul");
     let children33 = f33.children()
 
@@ -173,20 +194,20 @@ $(function () {
 
         }
     })
-
+    //j34----------------------------
     let ref34Div = $(".ref-34-div")
     $(".j-34").click(function () {
-
         for (i = 0; i < ref34Div.length; i++) {
-
             $(ref34Div[i]).css('fontWeight', "bold")
         }
     })
 
+    //j35----------------------------
     let font35 = $("#35-font")
     $(".j-35").click(() => font35.css("fontWeight", "bold"))
     let list36None = $('[class*="list-36"][style*="display:none"]')
 
+    //j36----------------------------
     $('.j-36').click(function () {
 
         for (let i = 0; i < list36None.length; i++) {
@@ -194,7 +215,7 @@ $(function () {
         }
     })
 
-
+    //j37----------------------------
     $('.j-37').click(function () {
 
         let selected37 = $('.select-37 > option:checked')
@@ -206,7 +227,7 @@ $(function () {
         message(strSelected)
     })
 
-
+    //j38----------------------------
     $('.j-38').click(function () {
 
         $(".f-38-ul:first a").attr("href", "http://www.assemblerschool.com/")
@@ -214,19 +235,18 @@ $(function () {
 
     })
 
-
+    //j39----------------------------
     $('.j-39').click(function () {
 
         let input = $('input:first')
         window.alert(input.attr("value"))
     })
-
+    //j40----------------------------
     $('.j-40').click(function () {
         let inputRemove = $('li>span[style*="color:blue"]')
         for (let i = 0; i < inputRemove.length; i++) $(inputRemove[i]).remove()
 
     })
-
 
     setTimeout(function () {
 
